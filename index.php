a03321018ad8d1070b5454e8352a602f34ffa303
M. Homeister <micha@0x25.de>
2025-01-24 22:39:33 +0100

    test done

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php
-
-echo "Hello";
-echo "Hello";
-echo "Fixes #6";
-echo "Hello";
-echo "Working on bugs here.";
-echo "Working on bugs here.";
-echo "2nd commit";
-echo "2nd commit";
+// empty
